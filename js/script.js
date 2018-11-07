69d094fb66b36982bc43655baaca0033d67e5fbb
Scott <scottbadger@hotmail.co.uk>
2018-11-07 09:22:09 +0800

    Created script.js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var car = {
+	make:"Subaru",
+	model:"Legacy",
+	colour:"Black",
+	year:2004,
+	isTurnedOn: true,
+	numberOfWheels: 4,
+	seats: [
+		"seat 1",
+		"seat 2",
+		"seat 3",
+		"seat 4"
+	],
+	turnOn: function() {
+		this.isTurnedOn=true;
+	},
+	turnOff: function() {
+		this.isTurnedOn=false;
+	},
+	fly: function() {
+		alert("Fly");
+	},
+	switchCar: function(isOn) {
+		console.log("Turn car " + isOn);
+		if(isOn == true) {
+			this.isTurnedOn=true;
+		} else {
+			this.isTurnedOn=false;
+		}
+	}
+};
+console.log("Hello there friends!!!!");
